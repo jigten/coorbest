0b33301cc1bca1e3c5fee6278639d7e436b78a01
Ugyen Jigten <ujigten@gmail.com>
2020-03-17 13:51:40 +0700

    Update to support multiple test groups

--- R/meandifcor.R
+++ R/meandifcor.R
@@ -8,37 +8,66 @@ meandifcor <- function(d) {
   cnt = 1
 
   if (startsWith(names(d$Values)[2], "Control")) {
+
+    group = c(rep("control", 1000))
+
+    cor_vals_b = c(d$Values$Control_Big)
+    cor_vals_m = c(d$Values$Control_Medium)
+    cor_vals_s = c(d$Values$Control_Small)
+
     grps <- split(d$Values, ceiling(seq_along(d$Values)/3))
     for (grp in grps[-1]) {
-      cor_vals_b = c(grp[[1]], d$Values$Control_Big)
-      cor_vals_m = c(grp[[2]], d$Values$Control_Medium)
-      cor_vals_s = c(grp[[3]], d$Values$Control_Small)
 
-      group = c(rep("test", 1000), rep("control", 1000))
+      group = c(group, rep(sprintf("test_%s", cnt), 1000))
 
-      corr_data_b = data.frame(group, cor_vals_b)
-      corr_data_m = data.frame(group, cor_vals_m)
-      corr_data_s = data.frame(group, cor_vals_s)
-
-      res[[sprintf("Test_%s_Big", cnt)]] <- dabestr::dabest(corr_data_b, group, cor_vals_b, idx=c("control", "test"), paired = FALSE)
-      res[[sprintf("Test_%s_Medium", cnt)]] <- dabestr::dabest(corr_data_m, group, cor_vals_m, idx=c("control", "test"), paired = FALSE)
-      res[[sprintf("Test_%s_Small", cnt)]] <- dabestr::dabest(corr_data_s, group, cor_vals_s, idx=c("control", "test"), paired = FALSE)
+      cor_vals_b = c(cor_vals_b, grp[[1]])
+      cor_vals_m = c(cor_vals_m, grp[[2]])
+      cor_vals_s = c(cor_vals_s, grp[[3]])
 
       cnt = cnt + 1
     }
+
+    corr_data_b = data.frame(group, cor_vals_b)
+    corr_data_m = data.frame(group, cor_vals_m)
+    corr_data_s = data.frame(group, cor_vals_s)
+
+    grp_arr = c("control")
+    test_id = 1
+
+    while (count >= 1) {
+      count = count - 1
+      grp_arr = c(grp_arr, sprintf("test_%s", test_id))
+      test_id = test_id + 1
+    }
+
+    res[["Test_Big"]] <- dabest(corr_data_b, group, cor_vals_b, idx=grp_arr, paired = FALSE)
+    res[["Test_Medium"]] <- dabest(corr_data_m, group, cor_vals_m, idx=grp_arr, paired = FALSE)
+    res[["Test_Small"]] <- dabest(corr_data_s, group, cor_vals_s, idx=grp_arr, paired = FALSE)
+
     return(res)
 
   } else {
+    group = c(rep("control", 1000))
+    cor_vals = c(d$Values$Control)
+
     for(t in d$Values[-1]) {
-      cor_vals = c(t, d$Values$Control)
-      group = c(rep("test", 1000), rep("control", 1000))
-
-      corr_data = data.frame(group, cor_vals)
-
-      res[[sprintf("Test %s", cnt)]] <- dabestr::dabest(corr_data, group, cor_vals, idx=c("control", "test"), paired = FALSE)
+      cor_vals = c(cor_vals, t)
+      group = c(group, rep(sprintf("test_%s", cnt), 1000))
 
       cnt = cnt + 1
     }
+
+    grp_arr = c("control")
+    test_id = 1
+
+    while (count >= 1) {
+      count = count - 1
+      grp_arr = c(grp_arr, sprintf("test_%s", test_id))
+      test_id = test_id + 1
+    }
+    corr_data = data.frame(group, cor_vals)
+    res[["Test"]] <- dabest(corr_data, group, cor_vals, idx=grp_arr, paired = FALSE)
+
     return(res)
   }
 }
